e623d881f39743d7f2ebd9d6cafdff06c6c36d90
bebadinia <142532011+bebadinia@users.noreply.github.com>
2023-09-19 01:29:58 -0500

    Add files via upload

---
+++ C/Header-File/MainTester.c
@@ -0,0 +1,30 @@
+/*
+ ==========================================================================
+ = 				-C header file written by Benyamin Ebadinia-              =
+ =                      -CS1311BN Programming 2-                          =
+ =                          -April 2021-         	                      =
+ =                                                                        =
+ =  			 *This program is a main tester for tutor*     		      =
+ =																          =
+ =              		     			                            	  =
+ =				    ___       ___				  						  =
+ =			       /   \ ~~~ /   \				  						  =
+ =			 ,----(       . .     )				  						  =
+ =			/      \___    	  ___/				  						  =
+ =		   /|	       (\    |(				  							  =
+ =		  ^ \   /___\   /\   |				  							  =
+ =			 |  |   |  |__\  |				  							  =
+ =			|__|   |__|    "" 				  							  =
+ ==========================================================================
+*/
+#include <stdio.h>
+#include <conio.h>
+#include "cmain.h"
+#include "function.h"
+
+
+void main (void)
+{
+	CMain();
+	Functions();
+}
