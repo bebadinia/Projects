814177cdd9e333c17a386b4d06069180b115786a
bebadinia <142532011+bebadinia@users.noreply.github.com>
2023-09-19 01:33:48 -0500

    Update MainTester.c

--- C/Header-File/MainTester.c
+++ C/Header-File/MainTester.c
@@ -1,22 +1,20 @@
 /*
  ==========================================================================
- = 				-C header file written by Benyamin Ebadinia-              =
- =                      -CS1311BN Programming 2-                          =
- =                          -April 2021-         	                      =
+ = 		-C header file written by Benyamin Ebadinia-              =
+ =                          -April 2021-         	                  =
  =                                                                        =
- =  			 *This program is a main tester for tutor*     		      =
- =																          =
- =              		     			                            	  =
- =				    ___       ___				  						  =
- =			       /   \ ~~~ /   \				  						  =
- =			 ,----(       . .     )				  						  =
- =			/      \___    	  ___/				  						  =
- =		   /|	       (\    |(				  							  =
- =		  ^ \   /___\   /\   |				  							  =
- =			 |  |   |  |__\  |				  							  =
- =			|__|   |__|    "" 				  							  =
+ =  		 *This program is a main tester for tutor*     	          =
+ =		        	___       ___				  =
+ =			       /   \ ~~~ /   \				  =
+ =			 ,----(       . .     )				  =
+ =			/      \___    	  ___/				  =
+ =		      /|           (\    |(	 			  =
+ =		      ^ \   /___\   /\   |				  =
+ =			 |  |   |  |__\  |				  =
+ =			 |__|   |__|    "" 				  =
  ==========================================================================
 */
+
 #include <stdio.h>
 #include <conio.h>
 #include "cmain.h"
